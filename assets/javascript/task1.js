0667f27bd29413027b13a95781f6aec23fc2aa66
AustinG <austin.geisler@vtsd.com>
2024-12-09 09:04:02 -0500

    Task 1 complete in task1.js and index.html

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,12 @@
 /* Put code for the task below */
+
+/* Function to summon button upon hovering over the image */
+function taskOneImgHover(){
+    document.getElementById("buttonTask1").style.visibility = "visible";
+}
+
+/* Function to hide the image by clicking aforementioned button */
+function taskOneButton(){
+    //Used visibility hidden so it wouldn't rearrange the webpage upon disappearing
+    document.getElementById("imageTask1").style.visibility = "hidden";
+}
