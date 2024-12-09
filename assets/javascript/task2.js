c7daa494585539c49cd417934612189d5067abf8
AustinG <austin.geisler@vtsd.com>
2024-12-09 09:23:37 -0500

    Task 2 completed in styles.css, index.html and task2.js (made 4 buttons because I wasn't sure if the white one would count)

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -6,3 +6,30 @@ Create 3 buttons (similar to the one provided for "Task 1")
 each associated with a different color. When each button is clicked,
 change the background of the table to the color the button represents.
  */
+
+//Bg White function
+function tableBgWhite(){
+    // Abstracting part of the code to make things easier to look at
+    table = document.getElementById("tableTask2")
+    table.style.backgroundColor = "white";
+}
+
+//Bg Cyan/Blue function
+function tableBgCyan(){
+    // Abstracting part of the code to make things easier to look at
+    table = document.getElementById("tableTask2")
+    table.style.backgroundColor = "cyan";
+}
+
+//Bg Yellow function
+function tableBgYellow(){
+    // Abstracting part of the code to make things easier to look at
+    table = document.getElementById("tableTask2")
+    table.style.backgroundColor = "yellow";
+}
+
+function tableBgMagenta(){
+    // Abstracting part of the code to make things easier to look at
+    table = document.getElementById("tableTask2")
+    table.style.backgroundColor = "magenta";
+}
