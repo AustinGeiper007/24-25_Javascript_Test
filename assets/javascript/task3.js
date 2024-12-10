70f0700b1e1c44a95728e8d2b068611d902ea11d
AustinG <austin.geisler@vtsd.com>
2024-12-10 08:05:07 -0500

    Task 3 complete in task3.js and index.html

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -6,3 +6,7 @@ Add a button to the navigation bar which,
 when clicked changes the text within the basic directions
 to "I Clicked It!".
  */
+
+function task3NavButton(){
+    document.getElementById("basicdirections").innerText = "I CLICKED IT!!!"
+}
