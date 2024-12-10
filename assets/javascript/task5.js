0ee53305ffe1f00e1a96258a78053ee009a4043d
AustinG <austin.geisler@vtsd.com>
2024-12-10 08:26:22 -0500

    Task 5 sort of completed. Instead of the boxes being read a text shows up saying one or more fields are empty

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -6,3 +6,15 @@ On the form add validation functionality that shows the form field
 (or its label) as red if the field is empty.
 They should turn back to white when the data is entered.
  */
+
+function task5Checker(){
+    let name = document.getElementById("fullName").value;
+    let address = document.getElementById("address").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+
+    if (name == "" || address == "" || phone == "" || email == "")
+        document.getElementById("invalidTask5").style.visibility = "visible";
+    else
+        document.getElementById("invalidTask5").style.visibility = "hidden";
+}
